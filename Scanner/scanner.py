01b17bc9725fa66615632fea7867048212e819b5
Turmoil <1853073118@qq.com>
2023-01-09 21:39:21 +0800

    modified something

--- Scanner/scanner.py
+++ Scanner/scanner.py
@@ -5,57 +5,73 @@ from fpdf import FPDF
 ret = str
 
 def search_images(path):
-    ''' Search all images, just directory. '''
+    """
+    Search all images, just directory.
+    """
 
-    imageList = []
+    image_list = []
     print("Searching...")
     for img in os.listdir(path):
         if (img[-3:] == "jpg") or (img[-3:] == "png"):
-            imageList.append(img)
-    if len(imageList) != 0:
+            image_list.append(img)
+    if len(image_list) != 0:
         print("Search Complete!")
         print("Images to convert:")
-        imageList.sort()
-        for dir in imageList:
-            print(f"\t {dir}")
+        image_list.sort()
+        for image in image_list:
+            print(f"\t {image}")
     else:
         print("No image!")
 
-    return imageList
+    return image_list
 
-def convert_pdf(path, filename, imageList):
-    ''' Convert all images to PDF. '''
 
-    print("Converting...")
+def create_pdf():
     pdf = FPDF()
     pdf.set_creator("Tony's Studio")
     pdf.set_auto_page_break(False)
-    for dir in imageList:
+
+    return pdf
+
+
+def convert_pdf(path, filename, image_list):
+    """
+    Convert all images to PDF.
+    """
+
+    if len(image_list) == 0:
+        return
+
+    print("Converting...")
+
+    pdf = create_pdf()
+    for img_dir in image_list:
         pdf.add_page()
-        pdf.image(os.path.join(path, dir), x = 0, y = 10, w = 210)
+        pdf.image(os.path.join(path, img_dir), x = 0, y = 10, w = 210)
     pdf.output(os.path.join(path, filename), "F")
     pdf.close()
-    print("Convertion Complete!\n")
+    print("Conversion Complete!\n")
+
 
 while True:
     print("------------- Copyright(C) Tony's Studio -------------")
     print("----------------- Image to PDF 1.1.0 -----------------")
-    print("-------------- Only support jpg and png --------------\n")
+    print("-------------- Only support jpg and png --------------")
 
-    # Read folder and filename.
-    path = input("Please enter the source folder: ").strip()
-    if path == "quit":
+    # Read source folder.
+    path = input("Please enter the source folder(\"q\" to quit): ").strip()
+    if path == "q":
         break
-    filename = input("Please enter the target filename: ").strip()
-    if filename == "quit":
+
+    # Read file name.
+    filename = input("Please enter the target filename(\"q\" to quit): ").strip()
+    if filename == "q":
         break
     if not filename.endswith(".pdf"):
         filename += ".pdf"
-    
-    # Search images
-    imageList = search_images(path)
-    if len(imageList) != 0:
-        convert_pdf(path, filename, imageList)
+
+    # Search and convert.
+    convert_pdf(path, filename, search_images(path))
         
     ret = input("Continue? (Y/N) ").lower()
     if ret[0] != 'y':
